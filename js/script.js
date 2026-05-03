b5d179197b8a7e32d2e3ece04e10a75c318b76df
fx420 <fx420fx4200@gmail.com>
2026-05-03 20:06:26 +0800

    v1.1

--- js/script.js
+++ js/script.js
@@ -3,38 +3,45 @@ const overlay = document.getElementById("overlay");
 const menuBtn = document.getElementById("menuBtn");
 const footerSlot = document.getElementById("footer-slot");
 
+// Load sidebar
 async function loadSidebar() {
   if (!sidebarSlot) return;
   const res = await fetch("component/sidebar.html");
   sidebarSlot.innerHTML = await res.text();
 }
 
+// Load footer
 async function loadFooter() {
   if (!footerSlot) return;
   const res = await fetch("component/footer.html");
   footerSlot.innerHTML = await res.text();
 }
-loadFooter();
 
+// Close sidebar
 function closeSidebar() {
   const sidebar = document.getElementById("sidebar");
   if (sidebar) sidebar.classList.remove("open");
-  if (overlay) overlay.classList.remove("show");
+  overlay?.classList.remove("show");
 }
 
+// Open sidebar
 menuBtn?.addEventListener("click", () => {
   const sidebar = document.getElementById("sidebar");
   if (sidebar) sidebar.classList.add("open");
-  if (overlay) overlay.classList.add("show");
+  overlay?.classList.add("show");
 });
 
+// Click overlay to close
 overlay?.addEventListener("click", closeSidebar);
 
+// Load components
+loadFooter();
 loadSidebar().then(() => {
   const currentPage = document.body.dataset.page;
 
   document.querySelectorAll(".nav a").forEach((link) => {
     const href = link.getAttribute("href");
+
     if (
       (currentPage === "about" && href === "index.html") ||
       (currentPage === "portfolio" && href === "portfolio.html") ||
